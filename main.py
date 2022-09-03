fc5314d7f79765ec83c6b3b863f750666bf997d5
krisian444 <bargas@ualberta.ca>
2022-09-03 17:04:32 -0600

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+# Randomly recommends anime from a list
+
+from random import choice
+import numpy as np
+import pandas as pd
+
+# list of anime
+anime = ['Hellsing Ultimate', 'One Piece', 'Kaguya-Sama', 'Attack on Titan', 'The World God Only Knows']
+
+#
+print(choice(anime))
